3f35b7121de7f1666baf5b03a096fe50c3eb39a6
Bo Yuan <bo.yuan29@hotmail.com>
2017-05-05 16:31:03 +0800

    init

---
+++ vmaster/api/openstack.py
@@ -0,0 +1,14 @@
+import api
+
+api_version_map = {
+    "2.0": [
+    ], 
+
+    "2.1": [
+        {"uri":"/servers/", "category":"servers", "view":"", "methods": [
+            {"name":"get", "param_fmt": {}, "response_fmt":{}, "doc":""}, 
+            {"name":"post", "param_fmt": {}, "response_fmt":{}, "doc":""},
+            {"name":"delete", "param_fmt": {}, "response_fmt":{}, "doc":""},
+        ]}, 
+    ],
+}
